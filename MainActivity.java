2e5c368a575a30036610cd09c811d7c6fda11222
Anonlun <153767125+Anonlun@users.noreply.github.com>
2023-12-13 20:19:51 +0100

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,43 +1,42 @@
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
+public class MainActivity extends AppCompatActivity {
 
-class MainActivity : AppCompatActivity() {
+    private EditText hoursWorkedEditText;
+    private EditText hourlyRateEditText;
+    private Button calculateButton;
+    private TextView resultTextView;
 
-    private lateinit var hoursWorkedEditText: EditText
-    private lateinit var hourlyRateEditText: EditText
-    private lateinit var calculateButton: Button
-    private lateinit var resultTextView: TextView
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        hoursWorkedEditText = findViewById(R.id.editTextHoursWorked);
+        hourlyRateEditText = findViewById(R.id.editTextHourlyRate);
+        calculateButton = findViewById(R.id.buttonCalculate);
+        resultTextView = findViewById(R.id.textViewResult);
 
-        hoursWorkedEditText = findViewById(R.id.editTextHoursWorked)
-        hourlyRateEditText = findViewById(R.id.editTextHourlyRate)
-        calculateButton = findViewById(R.id.buttonCalculate)
-        resultTextView = findViewById(R.id.textViewResult)
-
-        calculateButton.setOnClickListener { calculatePayment() }
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculatePayment();
+            }
+        });
     }
 
-    private fun calculatePayment() {
-        val hoursWorked = hoursWorkedEditText.text.toString().toDoubleOrNull() ?: 0.0
-        val hourlyRate = hourlyRateEditText.text.toString().toDoubleOrNull() ?: 0.0
+    private void calculatePayment() {
+        double hoursWorked = Double.parseDouble(hoursWorkedEditText.getText().toString());
+        double hourlyRate = Double.parseDouble(hourlyRateEditText.getText().toString());
 
-        val dailyPayment = hoursWorked * hourlyRate
-        val weeklyPayment = dailyPayment * 7
-        val monthlyPayment = dailyPayment * 30
+        double dailyPayment = hoursWorked * hourlyRate;
+        double weeklyPayment = dailyPayment * 7;
+        double monthlyPayment = dailyPayment * 30;
 
-        val resultText =
-            "Journalier: $dailyPayment €\nHebdomadaire: $weeklyPayment €\nMensuel: $monthlyPayment €"
-
-        resultTextView.text = resultText
-        resultTextView.visibility = View.VISIBLE
-    }
-}
-```
+        String resultText = String.form
+            
